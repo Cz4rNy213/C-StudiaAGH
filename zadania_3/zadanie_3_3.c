855d2715cd1ca5c65f22849f8bbd481b92ee1a42
Mikołaj Kołek <kolemiko@charon.kis.agh.edu.pl>
2023-10-25 16:44:19 +0200

    zadanie 3.3 gotowe

--- zadania_3/zadanie_3_3.c
+++ zadania_3/zadanie_3_3.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <math.h>
+
 /* Nalezy uzupelnic o header biblioteki zawierajacy definicja stalej INT_MAX */
 
 #define OCENIAJ 0  // Przed wyslaniem programu do oceny nalezy wartosc tej stalej zmienic na 1 
@@ -18,10 +20,16 @@ int main(void) {
         for (int i = 0; i < n; i++){
             nums[i] = (rand() % (999 - 100 + 1) + 100);
         }
-        for (int j = 1; j <= 9; j++){
-            for (int x = 0; x < n; x++){
-                
+        for (int first_digit = 1; first_digit <= 9; first_digit++){
+            double counter = 0, sum = 0;
+            for (int num = 0; num < n; num++){
+                if (floor(nums[num] / 100) == first_digit){
+                    counter ++;
+                    sum += nums[num];
+                }
             }
+            average = sum / counter;
+            printf("%.2lf ", average);
         }
     }
     
